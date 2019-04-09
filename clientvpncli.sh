4fada95528211dfdca67a60a113bf8a07d9ade9a
Jimmy <jclem1030@gmail.com>
2019-04-09 15:17:58 -0400

    Fixing more formatting issues and making the awk command portable between Mac and Linux

--- clientvpncli.sh
+++ clientvpncli.sh
@@ -42,53 +42,53 @@ check_easyrsa_install () {
 }
 
 install_easyrsa () {
-	#Ensure we're in the home directory
-	cd $HOME
+    #Ensure we're in the home directory
+    cd $HOME
 	
-	#Check if git is installed. If it is, proceed with installation
-	if [ -x "$(command -v git)" ]; then
-	    echo "git is installed. Proceeding with installation!"
-		git clone https://github.com/OpenVPN/easy-rsa.git
-	else
-		echo >&2 "This script depends on git. Please install git using your distribution's package manager."
-		exit 1
-	fi
+    #Check if git is installed. If it is, proceed with installation
+    if [ -x "$(command -v git)" ]; then
+        echo "git is installed. Proceeding with installation!"
+        git clone https://github.com/OpenVPN/easy-rsa.git
+    else
+        echo >&2 "This script depends on git. Please install git using your distribution's package manager."
+        exit 1
+    fi
 }
 
 check_pki () {
-	#Check if root CA certificate exists
-	if [ -e $PKI ]; then
-		echo "Root CA certificate exists. Continuing."
-	#If it doesn't, initialize the PKI
-	else
-		echo >&2 "Root CA doesn't exist. Creating PKI!"
-		check_easyrsa_install
-		build_pki
-	fi
+    #Check if root CA certificate exists
+    if [ -e $PKI ]; then
+        echo "Root CA certificate exists. Continuing."
+    #If it doesn't, initialize the PKI
+    else
+        echo >&2 "Root CA doesn't exist. Creating PKI!"
+        check_easyrsa_install
+        build_pki
+    fi
 }
 
 build_pki () {
-	#Switch to EasyRSA directory
-	$easyrsa
+    #Switch to EasyRSA directory
+    $easyrsa
 
-	#Read input for private key encryption
-	read -p "Would you like your root CA's private key to be encrypted? (Y/N) " answer
+    #Read input for private key encryption
+    read -p "Would you like your root CA's private key to be encrypted? (Y/N) " answer
 
-	#Initialize PKI and root CA. Decide whether or not to encrypt the CA's private key
-	case $answer in
-		Y|y)
-			./easyrsa init-pki
-			./easyrsa build-ca
-			;;
-		N|n)
-			./easyrsa init-pki
-			./easyrsa build-ca nopass
-			;;
-		*)
-			echo >&2 "Input \""$answer"\" not recognized. Aborting."
-			exit 1
-			;;
-	esac
+    #Initialize PKI and root CA. Decide whether or not to encrypt the CA's private key
+    case $answer in
+        Y|y)
+            ./easyrsa init-pki
+            ./easyrsa build-ca
+            ;;
+        N|n)
+            ./easyrsa init-pki
+            ./easyrsa build-ca nopass
+            ;;
+        *)
+            echo >&2 "Input \""$answer"\" not recognized. Aborting."
+            exit 1
+            ;;
+    esac
 }
 
 build_server () {
@@ -138,7 +138,7 @@ build_client () {
 update_config () {
 	#Prompt user with $CONFDIR contents
 	printf "\nContents of your configuration directory:\n----------"
-	ls -l $CONFDIR | awk '{print $10}'
+	ls -lQ $CONFDIR | awk -F\" '{print $2}'
 	printf "\n"
 
 	#Read input for config filename and check if it exists
@@ -147,7 +147,7 @@ update_config () {
 
 	#Prompt user with $ISSUED contents
 	printf "\nContents of your issued certificates directory:\n----------"
-	ls -l $ISSUED | awk '{print $10}'
+	ls -l $ISSUED | awk -F\" '{print $2}'
 	printf "\n"
 
 	#Read input for client name
@@ -159,88 +159,88 @@ update_config () {
 }
 
 check_config () {
-	#Check if config file exists in home directory
-	if [ -e "${CONFDIR}/${1}".ovpn ]; then
+    #Check if config file exists in home directory
+    if [ -e "${CONFDIR}/${1}".ovpn ]; then
         echo "${CONFDIR}/${1}.ovpn found."
-	else
+    else
         echo >&2 "${1}.ovpn not found in ${CONFDIR}. Aborting."
-		exit 1
-	fi
+        exit 1
+    fi
 }
 
 mutual_auth () {
-	#Check if the certificate and private key exist. Append values to the client config file. 
-	if [[ -e ${ISSUED}${2}.crt && -e ${PRIVATE}${2}.key ]]; then
-	    cert="$(openssl x509 -in ${ISSUED}${2}.crt -outform PEM)"
-		key="$(cat ${PRIVATE}${2}.key)"
-		printf "<cert>\n$cert\n</cert>\n<key>\n$key\n</key>\n" >> ${CONFDIR}/${1}.ovpn
-		echo "Certificate and private key appended to ${CONFDIR}/${1}.ovpn"
-	else
-		echo >&2 "Certificate and private key named \""${2}"\" do not exist."
-		exit 1
-	fi	
+    #Check if the certificate and private key exist. Append values to the client config file. 
+    if [[ -e ${ISSUED}${2}.crt && -e ${PRIVATE}${2}.key ]]; then
+        cert="$(openssl x509 -in ${ISSUED}${2}.crt -outform PEM)"
+        key="$(cat ${PRIVATE}${2}.key)"
+        printf "<cert>\n$cert\n</cert>\n<key>\n$key\n</key>\n" >> ${CONFDIR}/${1}.ovpn
+        echo "Certificate and private key appended to ${CONFDIR}/${1}.ovpn"
+    else
+        echo >&2 "Certificate and private key named \""${2}"\" do not exist."
+        exit 1
+    fi	
 }
 
 check_aws () {
-	#Check if AWS binary exists/is executable
-	if [ -x "$(command -v aws)" ]; then
+    #Check if AWS binary exists/is executable
+    if [ -x "$(command -v aws)" ]; then
         :
-	else
-		echo "AWS binary file does not exist. Please download AWS CLI."
-		exit 1
-	fi
+    else
+        echo "AWS binary file does not exist. Please download AWS CLI."
+        exit 1
+    fi
 }
 
 upload_cert () {
-	#Ensure AWS CLI is installed
-	check_aws
+    #Ensure AWS CLI is installed
+    check_aws
 
-	#Prompt user with $ISSUED contents
+    #Prompt user with $ISSUED contents
     printf "\nContents of your issued certificates directory:\n----------"
-    ls -l $ISSUED | awk '{print $10}'
-	printf "\n"
+    ls -l $ISSUED | awk -F\" '{print $2}'
+    printf "\n"
 
-	#Read input for cert filename and check if it exists
-	read -p "Enter the name of the certificate you would like to upload (omit .crt): " cert
+    #Read input for cert filename and check if it exists
+    read -p "Enter the name of the certificate you would like to upload (omit .crt): " cert
 
-	#Which region should we upload the cert to?
-	read -p "Which region would you like to upload the certificate to (e.g. us-east-1)? " region
+    #Which region should we upload the cert to?
+    read -p "Which region would you like to upload the certificate to (e.g. us-east-1)? " region
 
-	if [[ -e ${ISSUED}${cert}.crt && -e ${PRIVATE}${cert}.key ]]; then
-		aws acm import-certificate --certificate file://"${ISSUED}${cert}".crt --private-key file://"${PRIVATE}${cert}".key \
+    if [[ -e ${ISSUED}${cert}.crt && -e ${PRIVATE}${cert}.key ]]; then
+        aws acm import-certificate --certificate file://"${ISSUED}${cert}".crt --private-key file://"${PRIVATE}${cert}".key \
                 --certificate-chain file://"${PKI}" --region "$region" --output table
-	else
-		echo >&2 "Certificate and private key named \""${cert}"\" do not exist."
-		exit 1
-	fi
+    else
+        echo >&2 "Certificate and private key named \""${cert}"\" do not exist."
+        exit 1
+    fi
 }
 
 download_config () {
-	#Ensure AWS CLI is installed
-	check_aws
+    #Ensure AWS CLI is installed
+    check_aws
 
-	#List available CVPN endpoints
+    #List available CVPN endpoints
     read -p "Enter the region where your Client VPN endpoint is located (e.g. us-east-1): " region
-	aws ec2 describe-client-vpn-endpoints --region "$region" --query \
+    aws ec2 describe-client-vpn-endpoints --region "$region" --query \
             ClientVpnEndpoints[].'{Description:Description,EndpointID:ClientVpnEndpointId,ClientCIDR:ClientCidrBlock}' --output table
-	printf "\n"
+    printf "\n"
 
-	#Read input for CVPN endpoint ID
-	read -p "Enter your Client VPN endpoint ID: " id
+    #Read input for CVPN endpoint ID
+    read -p "Enter your Client VPN endpoint ID: " id
 	
-	#Read the name for the config file
-	read -p "Enter the name for your client configuration file: " config
+    #Read the name for the config file
+    read -p "Enter the name for your client configuration file: " config
 	
-	#Ensure AWS CLI is installed and make the API call to download the config
-	aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id "${id}" --output text>"${CONFDIR}/${config}".ovpn --region "$region"
+    #Ensure AWS CLI is installed and make the API call to download the config
+    aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id "${id}" --output text>"${CONFDIR}/${config}".ovpn --region "$region"
 
-	#Decide whether or not the above operation succeeded
-	if [ $? -eq 0 ]; then
-		echo "Configuration successfully saved to ${CONFDIR}/${config}.ovpn"
-	else
-		echo "An error occured. Exiting."
-		exit 1
-	fi
+    #Decide whether or not the above operation succeeded
+    if [ $? -eq 0 ]; then
+        echo "Configuration successfully saved to ${CONFDIR}/${config}.ovpn"
+    else
+        echo "An error occured. Exiting."
+        exit 1
+    fi
 }
 
 create_endpoint() {
