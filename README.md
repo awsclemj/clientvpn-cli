6c0de4ac8a505ff7bce3b5ea02e5a005d4d7865a
Jimmy <jclem1030@gmail.com>
2019-04-09 12:15:22 -0400

    Adding parity to the Client VPN AWS console. Getting closer to being able to interact fully from CLI Also modified some aspects of the original script for readability and usability purposes

--- README.md
+++ README.md
@@ -1,29 +1,49 @@
 # clientvpn-cli
 
-This script is meant to serve as a helper for the AWS Client VPN service. It makes it easy to manage certificates and update client configuration files for use with the service.
+1. [Introduction](#intro)
+2. [Goals](#goals)
+3. [Dependencies](#depends)
+4. [Options](#options)
+5. [Getting Started](#getstart)
+    5.1 [Script setup](#setup)
+    5.2 [Building a PKI](#pki)
+    5.3 [Uploading your certificates to Amazon Certificate Manager (ACM)](#upload)
+    5.4 [Downloading your client configuration file from AWS](#download)
+    5.5 [Appending mutual authentication parameters to the client configuration file](#mutual)
+    5.6 [Creating a Client VPN Endpoint](#create)
 
-## Goals
+## Introduction <a name="intro"></a>
+
+This script is meant to serve as a helper for the AWS Client VPN service. It makes it easy to manage certificates and update client configuration files for use with the service. You can also choose to create Client VPN endpoints and manage your route tables and authorization rules via this script.
+
+## Goals <a name="goals"></a>
 * Make the maintenance of a local PKI easy!
 * Generate root CA, server, and client certificates with simple CLI commands and guided input prompts
 * Easily upload server and client certificates to Amazon Certificate Manager
 * Append client certificate and private key to client configuration files when mutual authentication is used.
+* Create and manage Client VPN endpoints, route tables, and authorization rules
 
-## Dependencies
-* git
+## Dependencies <a name="depends"></a>
+* [git](https://github.com/git/git)
 * [EasyRSA](https://github.com/OpenVPN/easy-rsa) (and OpenSSL by proxy)
-* AWS CLI
+* [AWS CLI](https://github.com/aws/aws-cli)
 
-## Options
+## Options <a name="options"></a>
 * **--build-pki**: checks if EasyRSA is installed and builds your PKI. You can also issue this option to destroy and recreate your PKI.
 * **--build-server**: Build a server certificate and private key. You can optionally run this command first and skip --build-pki.
 * **--build-client**: Build a client certificate and private key. You can optionally run this command first and skip --build-pki.
 * **--upload-cert**: Upload your client and server certificates to ACM from the command line. 
 * **--download-config**: Download client configuration file from AWS.
 * **--update-config**: Run this option to update your client configuration file with the necessary OpenVPN directives for mutual authentication.
+* **--create-endpoint**: Create a Client VPN Endpoint in a specified region.
+* **--associate-subnet**: Associate a subnet to your Client VPN endpoint.
+* **--authorize-ingress**: Authorize a network for your ingress traffic. Optionally specify an Active Directory group to tie the authorization rule to.
+* **--create-route**: Add a route to your Client VPN route table. 
+## Getting Started <a name="getstart"></a>
 
-## Getting Started
+### Script setup <a name="setup"></a>
+You can install the script using git: `git clone https://github.com/ominousp0tat0/clientvpn-cli.git`
 
-### Script setup
 This script assumes that you have a default EasyRSA installation in the home directory for the user calling the script. If you don't the script can install it for you. 
 
 Please note the other dependencies above. git is used to install EasyRSA and AWS CLI is used for any actions that require interaction with AWS APIs. Please ensure that you have both installed and configured on your system.
@@ -32,7 +52,7 @@ There is one modifiable variable, CONFDIR, in the script file. You can choose to
 
 Take care to omit the trailing forward slash (/) from your directory if changing the CONFDIR variable. Some aspects of the script will not work if it's included. 
 
-### Building a PKI
+### Building a PKI <a name="pki"></a>
 You can issue `--build-pki`, `--build-server`, or `--build-client` as your first command to build your PKI. `--build-server` or `--build-client` will additionally set up a server or client certificate and private key, respectively, after setting up the PKI.
 
 If your system does not have EasyRSA installed, you can choose to have this script install it for you. You will be prompted with options to specify while your PKI is created, such as whether or not you want your private keys to be encrypted and what you'd like to name the files that are created.
@@ -51,11 +71,18 @@ Example order of operations:
 ```
 NOTE: Running `--build-pki` after having created a PKI on your system will destroy and recreate your entire PKI, including certs and keys located in the default directories. EasyRSA will prompt you before destroying the existing PKI, but use this option with caution.
 
-### Uploading your certificates to Amazon Certificate Manager (ACM)
+### Uploading your certificates to Amazon Certificate Manager (ACM) <a name="upload"></a>
 Run with `--upload-cert` to upload your client and/or server certificate(s) to ACM. You will be prompted asking that name of the certificate you'd like to upload
 
-### Downloading your client configuration file from AWS
+### Downloading your client configuration file from AWS <a name="download"></a>
 Run with `--download-config` to download your client configuration file from AWS. You will be prompted with which Client VPN endpoint you'd like to download the configuration for.
 
-### Appending mutual authentication parameters to the client configuration file
+### Appending mutual authentication parameters to the client configuration file <a name="mutual"></a>
 Run with `--update-config` to append your client certificate and private key to the .ovpn configuration file. Ensure to omit the file extension ".ovpn" or the script will not be able to locate the file.
+
+### Creating a Client VPN Endpoint <a name="create"></a>
+You can run the script with these options in the following order to create and set up a Client VPN endpoint:
+* **--create-endpoint**: Create a Client VPN Endpoint in a specified region.
+* **--associate-subnet**: Associate a subnet to your Client VPN endpoint.
+* **--authorize-ingress**: Authorize a network CIDR for your ingress traffic. Optionally specify an Active Directory group to tie the authorization rule to.
+* **--create-route**: Add a route to your Client VPN route table.
--- clientvpncli.sh
+++ clientvpncli.sh
@@ -16,30 +16,29 @@ FILE="$HOME/easy-rsa/easyrsa3/easyrsa"
 PKI="$HOME/easy-rsa/easyrsa3/pki/ca.crt"
 PRIVATE="$HOME/easy-rsa/easyrsa3/pki/private/"
 ISSUED="$HOME/easy-rsa/easyrsa3/pki/issued/"
-AWS="$HOME/.aws/credentials"
 easyrsa="cd $HOME/easy-rsa/easyrsa3"
 
 check_easyrsa_install () {
-#Ensure EasyRSA is in the user's home directory. If it's not, install it.
-if [ -e "$FILE" ]; then
+    #Ensure EasyRSA is in the user's home directory. If it's not, install it.
+    if [ -e "$FILE" ]; then
         echo "$FILE found."
-else
+    else
         read -p "$FILE not found... would you like me to install EasyRSA in $HOME? (Y/N) " answer
 	
-	case $answer in
-		Y|y)
-			install_easyrsa
-                        ;;
-                N|n)
-			echo "Please install the latest version of EasyRSA using git."
-			exit 1
-                        ;;
-                *)
-                        echo >&2 "Input \""$answer"\" not recognized. Aborting."
-                        exit 1
-                        ;;
-	esac
-fi
+	    case $answer in
+		    Y|y)
+		        install_easyrsa
+                ;;
+            N|n)
+		        echo "Please install the latest version of EasyRSA using git."
+		        exit 1
+                ;;
+            *)
+                echo >&2 "Input \""$answer"\" not recognized. Aborting."
+                exit 1
+                ;;
+	    esac
+    fi
 }
 
 install_easyrsa () {
@@ -48,7 +47,7 @@ install_easyrsa () {
 	
 	#Check if git is installed. If it is, proceed with installation
 	if [ -x "$(command -v git)" ]; then
-		echo "git is installed. Proceeding with installation!"
+	    echo "git is installed. Proceeding with installation!"
 		git clone https://github.com/OpenVPN/easy-rsa.git
 	else
 		echo >&2 "This script depends on git. Please install git using your distribution's package manager."
@@ -101,39 +100,39 @@ build_server () {
 	$easyrsa
 
 	case $answer in
-                Y|y)
-                        ./easyrsa build-server-full "$server"
-                        ;;
-                N|n)
-                        ./easyrsa build-server-full "$server" nopass
-                        ;;
-                *)
-                        echo >&2 "Input \""$answer"\" not recognized. Aborting."
-                        exit 1
-                        ;;
-        esac
+        Y|y)
+            ./easyrsa build-server-full "$server"
+            ;;
+        N|n)
+            ./easyrsa build-server-full "$server" nopass
+            ;;
+        *)
+            echo >&2 "Input \""$answer"\" not recognized. Aborting."
+            exit 1
+            ;;
+    esac
 }
 
 build_client () {
-        #Read input for client name and private key encryption
-        read -p "Enter the name for your client: " client
-        read -p "Would you like your client's private key to be encrypted? Note that encrypted keys cannot be uploaded to ACM. (Y/N) " answer
+    #Read input for client name and private key encryption
+    read -p "Enter the name for your client: " client
+    read -p "Would you like your client's private key to be encrypted? Note that encrypted keys cannot be uploaded to ACM. (Y/N) " answer
 
-        #Switch to EasyRSA directory and build a client cert & key based on above answer
-        $easyrsa
+    #Switch to EasyRSA directory and build a client cert & key based on above answer
+    $easyrsa
 
-        case $answer in
-                Y|y)
-                        ./easyrsa build-client-full "$client"
-                        ;;
-                N|n)
-                        ./easyrsa build-client-full "$client" nopass
-                        ;;
-                *)
-                        echo >&2 "Input \""$answer"\" not recognized. Aborting."
-                        exit 1
-                        ;;
-        esac
+    case $answer in
+        Y|y)
+            ./easyrsa build-client-full "$client"
+            ;;
+        N|n)
+            ./easyrsa build-client-full "$client" nopass
+            ;;
+        *)
+            echo >&2 "Input \""$answer"\" not recognized. Aborting."
+            exit 1
+            ;;
+    esac
 }
 
 update_config () {
@@ -152,7 +151,7 @@ update_config () {
 	printf "\n"
 
 	#Read input for client name
-        read -p "Enter the name of your client (omit .crt): " client
+    read -p "Enter the name of your client (omit .crt): " client
 	
 	#Pass values to function
 	mutual_auth "$config" "$client"
@@ -162,9 +161,9 @@ update_config () {
 check_config () {
 	#Check if config file exists in home directory
 	if [ -e "${CONFDIR}/${1}".ovpn ]; then
-        	echo "${CONFDIR}/${1}.ovpn found."
+        echo "${CONFDIR}/${1}.ovpn found."
 	else
-        	echo >&2 "${1}.ovpn not found in ${CONFDIR}. Aborting."
+        echo >&2 "${1}.ovpn not found in ${CONFDIR}. Aborting."
 		exit 1
 	fi
 }
@@ -172,7 +171,7 @@ check_config () {
 mutual_auth () {
 	#Check if the certificate and private key exist. Append values to the client config file. 
 	if [[ -e ${ISSUED}${2}.crt && -e ${PRIVATE}${2}.key ]]; then
-		cert="$(openssl x509 -in ${ISSUED}${2}.crt -outform PEM)"
+	    cert="$(openssl x509 -in ${ISSUED}${2}.crt -outform PEM)"
 		key="$(cat ${PRIVATE}${2}.key)"
 		printf "<cert>\n$cert\n</cert>\n<key>\n$key\n</key>\n" >> ${CONFDIR}/${1}.ovpn
 		echo "Certificate and private key appended to ${CONFDIR}/${1}.ovpn"
@@ -183,11 +182,11 @@ mutual_auth () {
 }
 
 check_aws () {
-	#Check if AWS credentials exist
-	if [ -e $AWS ]; then
-		echo "AWS credentials file exists! Continuing."
+	#Check if AWS binary exists/is executable
+	if [ -x "$(command -v aws)" ]; then
+        :
 	else
-		echo "AWS credentials file does not exist. Please download AWS CLI and/or run 'aws configure'"
+		echo "AWS binary file does not exist. Please download AWS CLI."
 		exit 1
 	fi
 }
@@ -197,8 +196,8 @@ upload_cert () {
 	check_aws
 
 	#Prompt user with $ISSUED contents
-        printf "\nContents of your issued certificates directory:\n----------"
-        ls -l $ISSUED | awk '{print $10}'
+    printf "\nContents of your issued certificates directory:\n----------"
+    ls -l $ISSUED | awk '{print $10}'
 	printf "\n"
 
 	#Read input for cert filename and check if it exists
@@ -208,7 +207,8 @@ upload_cert () {
 	read -p "Which region would you like to upload the certificate to (e.g. us-east-1)? " region
 
 	if [[ -e ${ISSUED}${cert}.crt && -e ${PRIVATE}${cert}.key ]]; then
-		aws acm import-certificate --certificate file://"${ISSUED}${cert}".crt --private-key file://"${PRIVATE}${cert}".key --certificate-chain file://"${PKI}" --region "$region" --output table
+		aws acm import-certificate --certificate file://"${ISSUED}${cert}".crt --private-key file://"${PRIVATE}${cert}".key \
+                --certificate-chain file://"${PKI}" --region "$region" --output table
 	else
 		echo >&2 "Certificate and private key named \""${cert}"\" do not exist."
 		exit 1
@@ -220,9 +220,9 @@ download_config () {
 	check_aws
 
 	#List available CVPN endpoints
-	read -p "Enter the region where your Client VPN endpoint is located: " region
-	printf "\nAvailable Endpoints:\n\nEndpoint ID\t\t\t\tCIDR Range\n------\t\t\t\t\t------\n"
-	aws ec2 describe-client-vpn-endpoints --region "$region" --output text | awk '/CLIENTVPNENDPOINTS/ {print $3,"\t",$2,"\t";}'
+    read -p "Enter the region where your Client VPN endpoint is located (e.g. us-east-1): " region
+	aws ec2 describe-client-vpn-endpoints --region "$region" --query \
+            ClientVpnEndpoints[].'{Description:Description,EndpointID:ClientVpnEndpointId,ClientCIDR:ClientCidrBlock}' --output table
 	printf "\n"
 
 	#Read input for CVPN endpoint ID
@@ -243,14 +243,244 @@ download_config () {
 	fi
 }
 
+create_endpoint() {
+    #Ensure AWS CLI is installed
+    check_aws
+
+    #Read inputs from user and create the endpoint
+    read -p "Enter a description for your Client VPN endpoint: " desc
+    read -p "Would you like your endpoint to use TCP or UDP for transport? (T/U) " proto
+    
+    #Set proto
+    case ${proto} in
+            T|t)
+                protocol="tcp"
+                ;;
+            U|u)
+                protocol="udp"
+                ;;
+            *)
+                echo >&2 "Input \""${proto}"\" not recognized. Aborting."
+                exit 1
+                ;;
+    esac
+
+    read -p "Enter your Client CIDR block (e.g. 192.168.0.0/16): " cidr
+    read -p "Enter the region you would like to create your Client VPN endpoint in (e.g. us-east-1): " region
+    aws acm list-certificates --output table --region "$region"
+    printf "\n"
+    read -p "Please select a server certificate ARN from the above list: " server
+    read -p "Would you like to use mutual authentication? (Y/N) " mutual
+
+    #Set initial API call
+    command="aws ec2 create-client-vpn-endpoint --description ${desc} --transport-protocol "${protocol}" --client-cidr-block "${cidr}" \
+            --server-certificate-arn "${server}" --output table"
+    
+    #Case statement for above decision
+    case ${mutual} in
+        Y|y)
+            aws acm list-certificates --output table --region "$region"
+            printf "\n"
+            read -p "Please select a client certificate ARN from the above list: " client
+            ;;
+        N|n)
+            ;;
+        *)
+            echo >&2 "Input \""${mutual}"\" not recognized. Aborting."
+            exit 1
+            ;;
+    esac
+
+    read -p "Would you like to use Active Directory authentication? (Y/N) " ad
+
+    #Case statement for above decision
+    case ${ad} in
+        Y|y)
+            aws ds describe-directories --output table --query \
+                    DirectoryDescriptions[].'{Description:Description,ID:DirectoryId,Name:Name,Type:Type,VPC:VpcSettings.VpcId}' --region "$region"
+            printf "\n"
+            read -p "Please select a directory ID from the above: " dir
+            ;;
+        N|n)
+            ;;
+        *)
+            echo >&2 "Input \""${ad}"\" not recognized. Aborting."
+            exit 1
+            ;;
+    esac
+
+    #Decision whether or not a form of authentication was specified
+    if [[ "${mutual}" == 'n' || "${mutual}" == 'N' ]] && [[ "${ad}" == 'n' || "${ad}" == 'N' ]]; then
+        echo >&2 "At least one form of authentication must be specified. Aborting."
+        exit 1
+    elif [[ "${mutual}" == 'n' || "${mutual}" == 'N' ]] && [[ "${ad}" == 'y' ||  "${ad}" == 'Y' ]]; then
+        command=""${command}" --authentication-options Type=directory-service-authentication,ActiveDirectory={DirectoryId="${dir}"}"
+    elif [[ "${mutual}" == 'y' || "${mutual}" == 'Y' ]] && [[ "${ad}" == 'n' || "${ad}" == 'N' ]]; then
+        command=""${command}" --authentication-options Type=certificate-authentication,MutualAuthentication={ClientRootCertificateChainArn="${client}"}"
+    else
+        command=""${command}" --authentication-options Type=directory-service-authentication,ActiveDirectory={DirectoryId="${dir}"} \
+                Type=certificate-authentication,MutualAuthentication={ClientRootCertificateChainArn="${client}"}"
+    fi
+
+    read -p "Would you like to enable CloudWatch Logs? (Y/N) " logs
+
+    #Case statement for above decision
+    case ${logs} in
+        Y|y)
+            aws logs describe-log-groups --output table --query logGroups[].'{Name:logGroupName}' --region "$region"
+            printf "\n"
+            read -p "Please select a log group from the above list: " group
+            aws logs describe-log-streams --log-group-name "${group}" --output table --query logStreams[].'{Name:logStreamName}' --region "$region"
+            printf "\n"
+            read -p "Please select a log stream from the above list: " stream
+            command=""${command}" --connection-log-options Enabled=true,CloudwatchLogGroup="${group}",CloudwatchLogStream="${stream}""
+            ;;
+        N|n)
+            command=""${command}" --connection-log-options Enabled=false"
+            ;;
+        *)
+            echo >&2 "Input \""${logs}"\" not recognized. Aborting."
+            exit 1
+            ;;
+    esac
+    
+    read -p "Would you like to use custom DNS servers? (Y/N) " dns
+
+    case ${dns} in
+        Y|y)
+            read -p "DNS server 1: " dns1
+            read -p "DNS server 2: " dns2
+            command=""${command}" --dns-servers "${dns1}" "${dns2}""
+            ;;
+        N|n)
+            ;;
+        *)
+            echo >&2 "Input \""${dns}"\" not recognized. Aborting."
+            exit 1
+            ;;
+    esac
+    
+    $command
+}
+
+associate_subnet() {
+    # Takes in user input and associates specified subnets to a CVPN endpoint
+    read -p "Please enter the region where your Client VPN endpoint resides (e.g. us-east-1) " region
+    aws ec2 describe-client-vpn-endpoints --region "$region" --query \
+            ClientVpnEndpoints[].'{Description:Description,EndpointID:ClientVpnEndpointId,ClientCIDR:ClientCidrBlock}' --output table
+    printf "\n"
+    read -p "Please choose your Client VPN endpoint: " endpoint
+    
+    bool=true
+    while [ "$bool" = true ]; do
+        aws ec2 describe-subnets --region "$region" --query \
+                Subnets[].'{SubnetID:SubnetId,CIDR:CidrBlock,AZ:AvailabilityZone,VPC:VpcId}' --output table
+        printf "\n"
+        read -p "Please choose the subnet ID you would like to associate: " subnet
+        aws ec2 associate-client-vpn-target-network --client-vpn-endpoint-id "$endpoint" --subnet-id "$subnet" --region "$region" --output table
+        printf "\n"
+        read -p "Would you like to associate another subnet? (Y/N) " answer
+        case ${answer} in
+            Y|y)
+                ;;
+            N|n)
+                bool=false
+                ;;
+            *)
+                echo >&2 "Input \""${answer}"\" not recognized. Aborting."
+                exit 1
+                ;;
+        esac
+    done         
+}
+
+authorize_ingress() {
+    read -p "Please enter the region where your Client VPN endpoint resides (e.g. us-east-1) " region
+    aws ec2 describe-client-vpn-endpoints --region "$region" --query \
+        ClientVpnEndpoints[].'{Description:Description,EndpointID:ClientVpnEndpointId,ClientCIDR:ClientCidrBlock}' --output table
+    printf "\n"
+    read -p "Please choose your Client VPN endpoint: " endpoint
+    
+    bool=true
+    while [ "$bool" = true ]; do
+        read -p "Please enter a destination CIDR to authorize (e.g. 192.168.0.0/16) " cidr
+        read -p "Will you be authorizing only for a specific Active Directory group? (Y/N) " ad
+        
+        case ${ad} in
+            Y|y)
+                read -p "Enter the ID of the Active Directory group: " id
+                aws ec2 authorize-client-vpn-ingress --client-vpn-endpoint-id "$endpoint" --target-network-cidr "$cidr" --access-group-id "$id" \
+                        --region "$region" --output table
+                ;;
+            N|n)
+                aws ec2 authorize-client-vpn-ingress --client-vpn-endpoint-id "$endpoint" --target-network-cidr "$cidr" --region "$region" \
+                    --authorize-all-groups --output table
+                ;;
+            *)
+                echo >&2 "Input \""${ad}"\" not recognized. Aborting."
+                exit 1
+                ;;
+
+        esac
+
+        read -p "Would you like to authorize another network? (Y/N) " answer
+
+        case ${answer} in
+            Y|y)
+                ;;
+            N|n)
+                bool=false
+                ;;
+            *)
+                echo >&2 "Input \""${answer}"\" not recognized. Aborting."
+                exit 1
+                ;;
+        esac
+    done
+}
+
+create_route() {
+    # Takes in input parameters and creates a route in the specified CVPN route table
+    read -p "Please enter the region where your Client VPN endpoint resides (e.g. us-east-1) " region
+    aws ec2 describe-client-vpn-endpoints --region "$region" --query \
+        ClientVpnEndpoints[].'{Description:Description,EndpointID:ClientVpnEndpointId,ClientCIDR:ClientCidrBlock}' --output table
+    printf "\n"
+    read -p "Please choose your Client VPN endpoint: " endpoint
+
+    bool=true
+    while [ "$bool" = true ]; do
+        read -p "Please enter a destination CIDR to add to your route table (e.g. 192.168.0.0/16) " cidr
+        aws ec2 describe-client-vpn-target-networks --client-vpn-endpoint-id "$endpoint" --region "$region" --query \
+            ClientVpnTargetNetworks[].'{EndpointID:ClientVpnEndpointId,SubnetID:TargetNetworkId,VPC:VpcId,Status:Status.Code}' --output table
+        printf "\n"
+        read -p "Please choose the subnet ID for the target of your route entry: " subnet
+        aws ec2 create-client-vpn-route --client-vpn-endpoint-id "$endpoint" --region "$region" --destination-cidr-block "$cidr" --target-vpc-subnet-id "$subnet" --output table
+        printf "\n"
+        read -p "Would you like to add an additional route? (Y/N) " answer
+
+        case ${answer} in
+            Y|y)
+                ;;
+            N|n)
+                bool=false
+                ;;
+            *)
+                echo >&2 "Input \""${answer}"\" not recognized. Aborting."
+                exit 1
+                ;;
+        esac
+    done
+}
+
+
 # Main execution starts here
-if [[ $# -eq 1 ]]; then
+if [ $# -eq 1 ]; then
 
 	case ${1} in
 		--build-pki)
-			check_easyrsa_install
-                	build_pki
-                	;;
+		    check_easyrsa_install
+            build_pki
+            ;;
 		--build-server)
 			check_pki
 			build_server
@@ -268,10 +498,22 @@ if [[ $# -eq 1 ]]; then
 		--download-config)
 			download_config
 			;;
+        --create-endpoint)
+            create_endpoint
+            ;;
+        --associate-subnet)
+            associate_subnet
+            ;;
+        --authorize-ingress)
+            authorize_ingress
+            ;;
+        --create-route)
+            create_route
+            ;;
 		*)
 			echo "${0}: Unknown argument \""${1}"\"" >&2
 			;;
 	esac
-else
+else 
 	echo >&2 "Invalid number of arguments passed."
 fi	
